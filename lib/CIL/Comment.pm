20267d8af37709eb13639d3c155b264adfef53bc
Andrew Chilton <andychilton@gmail.com>
2008-04-20 01:13:11 +1200

    Add the updates for CIL::Comment

--- lib/CIL/Comment.pm
+++ lib/CIL/Comment.pm
@@ -3,7 +3,22 @@ package CIL::Comment;
 
 use strict;
 use warnings;
+use Config::IniFiles;
+
 use base qw(CIL::Base);
+
+## ----------------------------------------------------------------------------
+
+sub new {
+    my ($proto) = @_;
+    my $class = ref $proto || $proto;
+    my $self = {};
+    $self->{data}    = {};
+    $self->{Changed} = 0;
+    bless $self, $class;
+    $self->inserted;
+    return $self;
+}
 
 ## ----------------------------------------------------------------------------
 1;
