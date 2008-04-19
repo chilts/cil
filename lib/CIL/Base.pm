9b53d15cf75e7bb44e8a83e2fb9c83304b01e8f5
Andrew Chilton <andychilton@gmail.com>
2008-04-19 21:57:45 +1200

    Added the commands init, add and list

--- lib/CIL/Base.pm
+++ lib/CIL/Base.pm
@@ -3,19 +3,59 @@ package CIL::Base;
 
 use strict;
 use warnings;
+use Carp;
 use DateTime;
 
 use base qw(Class::Accessor);
 __PACKAGE__->mk_accessors(qw(Description CreatedBy Inserted Updated));
 
+# override Class::Accessor's set
+sub set {
+    my ($self, $key, $value) = @_;
+    croak "provide a key name" unless defined $key;
+
+    my $orig = $self->get($key);
+
+    # get out if both are defined and they're the same
+    if ( defined $orig and defined $value ) {
+        return if $orig eq $value
+    }
+
+    # get out if neither are defined
+    if ( !defined $orig and !defined $value ) {
+        return;
+    }
+
+    # since we're actually changing the key, say we updated something
+    $self->{data}{$key} = $value;
+    $self->updated;
+}
+
+sub set_no_update {
+    my ($self, $key, $value) = @_;
+    croak "provide a key name" unless defined $key;
+    $self->{data}{$key} = $value;
+}
+
+# override Class::Accessor's get
+sub get {
+    my ($self, $key) = @_;
+    $self->{data}{$key};
+}
+
 sub inserted {
     my ($self) = @_;
-    $self->{Inserted} = DateTime->new()->iso8601;
+    my $time = time;
+    $self->{data}{Inserted} = $time;
+    $self->{data}{Updated} = $time;
+    $self->{Changed} = '1';
 }
 
 sub updated {
     my ($self) = @_;
-    $self->{Updated} = DateTime->new()->iso8601;
+    my $time = time;
+    $self->{data}{Updated} = $time;
+    $self->{Changed} = '1';
 }
 
 ## ----------------------------------------------------------------------------
--- lib/CIL/Issue.pm
+++ lib/CIL/Issue.pm
@@ -3,54 +3,93 @@ package CIL::Issue;
 
 use strict;
 use warnings;
+use Carp;
 use Data::Dumper;
 use Config::IniFiles;
+use YAML qw(LoadFile DumpFile);
 
 use base qw(CIL::Base);
 __PACKAGE__->mk_accessors(qw(Name Summary Status Labels Comments));
 
+my @ATTRS = ( qw(Name Summary Description CreatedBy Status Labels Comments) );
+
 ## ----------------------------------------------------------------------------
 
-sub load_issue {
-    my ($self, $name) = @_;
-    return unless defined $name;
+sub new {
+    my ($proto) = @_;
+    my $class = ref $proto || $proto;
+    my $self = {};
+    bless $self, $class;
+    $self->inserted;
+    return $self;
+}
 
-    my $filename = "issues/$name.ini";
+sub new_load_issue {
+    my ($class, $name) = @_;
 
-    return unless -f $filename;
+    unless ( defined $name ) {
+        croak "provide an issue name to load";
+    }
+
+    my $filename = "issues/$name.yaml";
+    unless ( -f $filename ) {
+        croak "filename '$filename' does no exist";
+    }
+
     my $issue = CIL::Issue->new();
-    $issue->read_issue( $filename );
-    $issue->Name( $name );
+    $issue->{data} = LoadFile( $filename );
+    return $issue;
+
+    my $cfg = Config::IniFiles->new( -file => $filename );
+    unless ( defined $cfg ) {
+        croak("not a valid inifile");
+    }
+
+    # my $issue = CIL::Issue->new();
+    foreach my $attr ( qw(Summary Name Description CreatedBy Status Labels Inserted Updated) ) {
+        # modify the data directly, otherwise Updated will kick in
+        $issue->{data}{$attr} = $cfg->val( 'Issue', $attr );
+    }
+    $issue->{data}{Comments}    = [];
+
+    # set the issue Name
+    $issue->{data}{Name} = $name;
+
     return $issue;
 }
 
-sub read_issue {
-    my ($self, $filename) = @_;
+sub new_parse_issue {
+    my ($class, $file) = @_;
 
-    my $cfg = Config::IniFiles->new( -file => $filename );
+    # $file may be a string ($filename) or a file handle ($fh)
+    my $cfg = Config::IniFiles->new( -file => $file );
 
-    foreach my $attr ( qw(Summary Description) ) {
-         $self->{$attr} = $cfg->val( 'Issue', $attr );
+    unless ( defined $cfg ) {
+        croak("not a valid inifile");
     }
+
+    my $issue = CIL::Issue->new();
+    foreach my $attr ( qw(Summary Name Description CreatedBy Status Labels Inserted Updated) ) {
+        # modify the data directly, otherwise Updated will kick in
+        $issue->set_no_update($attr, $cfg->val( 'Issue', $attr ));
+    }
+    $issue->set_no_update('Comments', []);
+    return $issue;
+}
+
+sub save {
+    my ($self) = @_;
+    my $name = $self->Name;
+    my $filename = "issues/$name.yaml";
+    DumpFile($filename, $self->{data});
 }
 
 sub reset {
     my ($self) = @_;
 
-    foreach my $attr ( qw(changed) ) {
+    foreach my $attr ( @ATTRS ) {
         delete $self->{$attr};
     }
-}
-
-sub inserted {
-    my ($self) = @_;
-    $self->{data}{inserted} = time;
-}
-
-sub updated {
-    my ($self) = @_;
-    $self->{changed} = '1';
-    $self->{data}{updated} = time;
 }
 
 sub add_comment {
